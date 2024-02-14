03e2ab9ec34539a12b2049d49e352fd372412ac6
Egor Fedorov <egor.fedorov@codestetic.com>
2024-02-14 17:35:54 +0300

    NB-1 Организация структуры проекта

--- NotifierBot/Configuration/Impl/BotSettings.cs
+++
@@ -1,8 +0,0 @@
-namespace NotifierBot.Configuration.Impl;
-
-/// <inheritdoc cref="IBotSettings" />
-internal class BotSettings : IBotSettings
-{
-    /// <inheritdoc />
-    public string Key { get; init; } = string.Empty;
-}
